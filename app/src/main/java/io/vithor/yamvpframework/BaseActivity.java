4408fec9e23e1bc7823a6d93797ce03396e9dc5b
Vithorio Polten <vithoriop@gmail.com>
2015-12-29 00:16:52 -0200

    First commit

---
+++ app/src/main/java/io/vithor/yamvpframework/BaseActivity.java
@@ -0,0 +1,12 @@
+package io.vithor.yamvpframework;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}

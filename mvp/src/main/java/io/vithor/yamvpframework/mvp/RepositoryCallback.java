615e368f36574c9531a9181745dac81dac6b2264
Vithorio Polten <vithoriop@gmail.com>
2016-06-15 15:09:14 -0300

    For the lint

--- mvp/src/main/java/io/vithor/yamvpframework/mvp/RepositoryCallback.java
+++ mvp/src/main/java/io/vithor/yamvpframework/mvp/RepositoryCallback.java
@@ -6,12 +6,17 @@ package io.vithor.yamvpframework.mvp;
 public interface RepositoryCallback<T, R, E extends Throwable> {
     /**
      * Successful HTTP response.
+     *
+     * @param t Response Body Deserialized
+     * @param response ResponseContainer
      */
     void success(T t, ResponseContainer<R> response);
 
     /**
      * Unsuccessful HTTP response due to network failure, non-2XX status code, or unexpected
      * exception.
+     *
+     * @param error ErrorContainer
      */
     void failure(ErrorContainer<E> error);
 }

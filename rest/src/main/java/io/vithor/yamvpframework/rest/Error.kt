ed56edb615da0ff49f1dd17be4e522643dde048f
Vithorio Polten <vithoriop@gmail.com>
2016-06-21 15:45:53 -0300

    A little bit more robust

--- rest/src/main/java/io/vithor/yamvpframework/rest/Error.kt
+++ rest/src/main/java/io/vithor/yamvpframework/rest/Error.kt
@@ -3,11 +3,13 @@ package io.vithor.yamvpframework.rest
 /**
  * Created by Guizion on 13/06/16.
  */
-
+import com.google.gson.JsonParseException
+import com.google.gson.JsonSyntaxException
+import io.vithor.yamvpframework.core.extensions.unwrap
+import io.vithor.yamvpframework.core.extensions.whenNull
 import okhttp3.Request
 import okhttp3.ResponseBody
 import retrofit2.Response
-import java.net.ConnectException
 
 /**
  * Created by Vithorio Polten on 1/4/16.
@@ -15,38 +17,76 @@ import java.net.ConnectException
 class Error(val errorType: ErrorType, val statusCode: HttpStatusCode = HttpStatusCode(0), val responseBody: ResponseBody? = null, val errorBody: String? = null, val request: Request? = null) {
 
     object Factory {
-        fun from(request: Request, t: Throwable): Error {
-            if (t is ConnectException) {
-                return Error(ErrorType.CannotReachServer)
-            } else if (t.cause is java.io.EOFException) {
-                return Error(ErrorType.CannotReachServer)
-            } else if (t.cause is java.net.UnknownHostException) {
-                return Error(ErrorType.CannotReachServer)
+        /**
+         * Make an Error instance from a Throwable instance.
+         *
+         * @return Error instance from request and throwable instances.
+         */
+        fun from(request: Request, throwable: Throwable): Error {
+            when(throwable) {
+                is java.net.SocketTimeoutException ->  {
+                    return Error(ErrorType.ClientTimeOut(), request = request)
+                }
+                is java.net.ConnectException,
+                is java.net.UnknownHostException,
+                is java.io.EOFException -> {
+                    return Error(ErrorType.CannotReachServer(), request = request)
+                }
+                is JsonParseException,
+                is JsonSyntaxException,
+                is IllegalAccessException -> {
+                    return Error(ErrorType.MarshalingFailed(throwable), request = request)
+                }
+                else -> {
+                    var error: Error? = null
+                    throwable.cause.unwrap { cause ->
+                        if (cause != throwable) {
+                            error = from(request, throwable = cause)
+                        }
+                    }.whenNull {
+                        error = Error(ErrorType.Unknown(throwable = throwable), request = request)
+                    }
+
+                    return error!!
+                }
             }
-            return Error(ErrorType.Unknown, request = request)
         }
 
+        /**
+         * Make an Error instance from a retrofit2.Response instance.
+         *
+         * @return Error instance from request and response instances.
+         */
         fun <T> from(request: Request, resp: Response<T>): Error {
             val raw = resp.raw()
             val code = HttpStatusCode(raw?.code() ?: 0)
             val errorType = when (code.description) {
-                HttpStatusCode.Status.InternalServerError -> ErrorType.InternalServerError
-                HttpStatusCode.Status.NetworkConnectTimeoutError -> ErrorType.TimeOut
-                HttpStatusCode.Status.Unauthorized -> ErrorType.Unauthorized
-                HttpStatusCode.Status.BadRequest -> ErrorType.BadRequest
-                else -> ErrorType.Unknown
+                HttpStatusCode.Status.InternalServerError -> ErrorType.InternalServerError()
+                HttpStatusCode.Status.NetworkConnectTimeoutError -> ErrorType.ServerTimeOut()
+                HttpStatusCode.Status.Unauthorized -> ErrorType.Unauthorized()
+                HttpStatusCode.Status.BadRequest -> ErrorType.BadRequest(request, resp)
+                else -> ErrorType.Unknown()
             }
             return Error(errorType, code, raw?.body(), resp.errorBody()?.string(), request = request)
         }
     }
 
-    enum class ErrorType {
-        Unknown,
-        CannotReachServer,
-        DeserializationFailed,
-        InternalServerError,
-        TimeOut,
-        Unauthorized,
-        BadRequest,
+
+    sealed class ErrorType {
+        class Unknown(val throwable: Throwable? = null) : ErrorType()
+
+        class CannotReachServer() : ErrorType()
+
+        class MarshalingFailed(val throwable: Throwable? = null) : ErrorType()
+
+        class InternalServerError() : ErrorType()
+
+        open class TimeOut() : ErrorType()
+        class ServerTimeOut() : TimeOut()
+        class ClientTimeOut() : TimeOut()
+
+        class Unauthorized() : ErrorType()
+
+        class BadRequest<T>(val request: Request, val response: Response<T>) : ErrorType()
     }
 }

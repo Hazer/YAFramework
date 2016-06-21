7dd97086147bb5445a4123034c160f244e260a84
Vithorio Polten <vithoriop@gmail.com>
2016-06-21 15:45:35 -0300

    Some docs and new extension

--- core/src/main/java/io/vithor/yamvpframework/core/extensions/CollectionExtensions.kt
+++ core/src/main/java/io/vithor/yamvpframework/core/extensions/CollectionExtensions.kt
@@ -4,14 +4,28 @@ package io.vithor.yamvpframework.core.extensions
  * Created by Hazer on 4/8/16.
  */
 
+/**
+ * Tries to cast this as? MutableList
+ *
+ * @return MutableList from Collection, or null in case cast failed.
+ */
 fun <E> Collection<E>?.asMutableList(): MutableList<E>? {
     return this as? MutableList<E>
 }
 
+/**
+ * @return A list with pairs of Index, Value.
+ */
 fun <T> Array<T>.enumerated(): List<Pair<Int, T>> {
     return indices.zip(this)
 }
 
+/**
+ * Execute {action} for each item of the collection, case {count} > 1.
+ * @param action Receives {item} from collection at {index}.
+ *
+ * @return itself
+ */
 inline fun <T> Collection<T>.caseMany(action: (Int, T) -> Unit): Collection<T> {
     if (count() > 1) {
         forEachIndexed { index, obj ->
@@ -21,6 +35,12 @@ inline fun <T> Collection<T>.caseMany(action: (Int, T) -> Unit): Collection<T> {
     return this
 }
 
+/**
+ * Execute {action} when collection only have 1 item.
+ * @param action Receives {first} item from collection.
+ *
+ * @return itself
+ */
 inline fun <T> Collection<T>.caseSingle(action: (T) -> Unit): Collection<T> {
     if (count() == 1) {
         action.invoke(first())
@@ -28,6 +48,12 @@ inline fun <T> Collection<T>.caseSingle(action: (T) -> Unit): Collection<T> {
     return this
 }
 
+/**
+ * Execute {action} when Iterable only have 1 item.
+ * @param action Receives {first} item from Iterable.
+ *
+ * @return itself
+ */
 inline fun <T> Iterable<T>.caseSingle(action: (T) -> Unit): Iterable<T> {
     if (count() == 1) {
         action.invoke(first())
@@ -35,7 +61,12 @@ inline fun <T> Iterable<T>.caseSingle(action: (T) -> Unit): Iterable<T> {
     return this
 }
 
-
+/**
+ * Execute {action} for each item of the Iterable, case {count} > 1.
+ * @param action Receives {item} from Iterable at {index}.
+ *
+ * @return itself
+ */
 inline fun <T> Iterable<T>.caseMany(action: (Int, T) -> Unit): Iterable<T> {
     if (count() > 1) {
         forEachIndexed { index, obj ->
--- core/src/main/java/io/vithor/yamvpframework/core/extensions/OptionalExtensions.kt
+++ core/src/main/java/io/vithor/yamvpframework/core/extensions/OptionalExtensions.kt
@@ -4,8 +4,28 @@ package io.vithor.yamvpframework.core.extensions
  * Created by Hazer on 3/10/16.
  */
 
-fun <T> T?.unwrap(action: (T) -> Unit) {
+/**
+ * Execute action closure with {this} as argument when this is not null.
+ * @param action invoke (T) -> Unit using {this} as argument if not null
+ *
+ * @return itself
+ */
+fun <T> T?.unwrap(action: (T) -> Unit): T? {
     if (this != null) {
         action.invoke(this)
     }
+    return this
+}
+
+/**
+ * Execute action closure when this is null.
+ * @param action invoke if null.
+ *
+ * @return itself
+ */
+fun <T> T?.whenNull(action: () -> Unit): T? {
+    if (this == null) {
+        action.invoke()
+    }
+    return this
 }

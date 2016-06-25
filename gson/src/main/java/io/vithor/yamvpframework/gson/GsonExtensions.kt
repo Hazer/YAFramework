4133b98c7385657792e830730e81878bb6add98e
Vithorio Polten <vithoriop@gmail.com>
2016-06-25 04:56:10 -0300

    Fixed and cleaned gson extensions

--- gson/src/main/java/io/vithor/yamvpframework/gson/GsonExtensions.kt
+++ gson/src/main/java/io/vithor/yamvpframework/gson/GsonExtensions.kt
@@ -8,26 +8,30 @@ import com.google.gson.JsonObject
  * Created by Vithorio Polten on 2/19/16.
  */
 
-fun JsonElement.objOrNull(): JsonObject? {
+fun JsonElement.asObjectOrNull(): JsonObject? {
     if (this.isJsonNull) return null
     return if (this.isJsonObject) this.asJsonObject else null
 }
 
-fun JsonElement.arrayOrNull(): JsonArray? {
+fun JsonElement.asArrayOrNull(): JsonArray? {
     if (this.isJsonNull) return null
     return if (this.isJsonArray) this.asJsonArray else null
 }
 
-
-fun JsonElement.getString(key: String): String? {
+fun JsonElement.asStringOrNull(): String? {
     if (this.isJsonNull) return null
     return if (this.isJsonPrimitive) this.asString else null
 }
 
+fun JsonElement.getString(key: String): String? {
+    return this.asObjectOrNull()?.getString(key)
+}
+
+fun JsonObject.getString(key: String): String? {
+    if (this.isJsonNull) return null
+    return this.get(key).asStringOrNull()
+}
+
 fun JsonElement.forEach(function: (json: JsonElement) -> Unit) {
-    if (!this.isJsonNull && this.isJsonArray) {
-        this.asJsonArray.forEach {
-            function(it)
-        }
-    }
+    this.asArrayOrNull()?.forEach(function)
 }

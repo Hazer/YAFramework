665cb75e0d69f7e4ebf1c8136020ec388cf83e32
Lucas Paim <lucasmpaim1@gmail.com>
2016-11-14 14:30:58 -0200

    Remove PersistableBundle references

--- ui/src/main/java/io/vithor/yamvpframework/ui/BaseActivity.kt
+++ ui/src/main/java/io/vithor/yamvpframework/ui/BaseActivity.kt
@@ -1,7 +1,6 @@
 package io.vithor.yamvpframework.ui
 
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.support.annotation.CallSuper
 import android.support.v7.app.AppCompatActivity
 import com.afollestad.assent.Assent
@@ -58,21 +57,10 @@ abstract class BaseActivity : AppCompatActivity() /*, PermissionDelegate*/ {
         super.onSaveInstanceState(outState)
     }
 
-    @CallSuper
-    override fun onSaveInstanceState(outState: Bundle?, outPersistentState: PersistableBundle?) {
-        savedInstanceStateCalled = true
-        super.onSaveInstanceState(outState, outPersistentState)
-    }
 
     @CallSuper
     override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
         super.onRestoreInstanceState(savedInstanceState)
-        savedInstanceStateCalled = false
-    }
-
-    @CallSuper
-    override fun onRestoreInstanceState(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onRestoreInstanceState(savedInstanceState, persistentState)
         savedInstanceStateCalled = false
     }
 
